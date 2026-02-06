cf1f39dd4a7e3dfa887d3407fbb33589af645dd3
Jacob <jacob.arrowsmith17@gmail.com>
2026-02-06 09:40:20 +0000

    added vercel analytic functions

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import SessionWrapper from '@/components/SessionWrapper';
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
